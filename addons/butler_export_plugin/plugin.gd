bb03be0ba01e7a28c6ea2fff9960198076c1cea3
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-03-23 19:21:45 -0500

    Assist script documentation loading
    Makes the scripts automatically load in the editor when appropriate, to make the editor documentation viewer more reliable for those scripts.

--- addons/butler_export_plugin/plugin.gd
+++ addons/butler_export_plugin/plugin.gd
@@ -5,7 +5,19 @@ extends EditorPlugin
 const PLUGIN_NAME := "butler_export_plugin"
 const PLUGIN_ICON := preload("./icon.svg")
 
+const _ENSURE_SCRIPT_DOCS:Array[Script] = [
+    preload("./butler_export_plugin.gd")
+]
+
 var _current_inst:ButlerExportPlugin = null
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs() -> void:
+	var edit := EditorInterface.get_script_editor()
+	for scr in _ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
 
 func _get_plugin_icon():
 	return PLUGIN_ICON
@@ -14,9 +26,11 @@ func _get_plugin_name():
 	return PLUGIN_NAME
 
 func _enter_tree():
+	_ensure_script_docs()
 	_try_init_plugin()
 
 func _enable_plugin():
+	_ensure_script_docs()
 	_try_init_plugin()
 
 func _disable_plugin():
